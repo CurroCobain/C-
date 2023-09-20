e3b25499c6451c34061f8d4e3df8006998fc4f13
GaelCobain <fmunmar310@g.educaand.es>
2023-09-20 13:00:29 +0200

    Nuevo ejercicio añadido

--- equalsOdd/Program.cs
+++ equalsOdd/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Kata
+{
+    public static String equalsOdd(int x, int y)
+    {
+        if (x == y)
+            return "equals";
+        else return "odd";
+    }
+        
+    public static void Main () {
+        System.Console.WriteLine(equalsOdd(10,7));
+        System.Console.WriteLine(equalsOdd(7, 7));
+
+    }
+}
+
---
+++ fibonacciLastDigit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+public class Kata
+{
+    public static int GetLastDigit(int index)
+    {
+        int num1 = 0;
+        int num2 = 1;
+        int rep = 0;
+        int numResult = 0;
+        string strNumResult = "";
+        var list = new List<int>();
+        while (rep < index) {
+            list.Add(numResult);
+            numResult = num1 + num2;
+            num1 = num2;
+            num2 = numResult;
+            rep++;
+            if (rep == index) 
+            {
+                strNumResult = System.Convert.ToString(list.Last());
+                strNumResult = strNumResult.Substring(strNumResult.Length - 1);
+                numResult = System.Convert.ToInt16(strNumResult);
+                return numResult;
+            }
+        }
+       
+        
+      return 1;
+    }
+    public static void Main()
+    {
+        System.Console.WriteLine(GetLastDigit(10));
+    }
+}
