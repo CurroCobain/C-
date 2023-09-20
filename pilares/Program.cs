6b08844333cd4c22ed711fcc2aeca3b6a09d0266
GaelCobain <fmunmar310@g.educaand.es>
2023-09-20 11:24:02 +0200

    Agregar archivos de proyecto.

---
+++ pilares/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+public class Kata
+{
+    public static  int Pillars( int numPill, int dist, int width)
+    {
+        if (numPill > 2)
+        {
+            return (((numPill - 1) * dist) * 10) - (width * 2);
+        }
+        else if (numPill == 2) 
+        {
+            return dist;
+        }
+        return 0;
+    }
+    public static void Main()
+    {
+        System.Console.WriteLine(Pillars(2, 10, 10));
+    }
+}
+
