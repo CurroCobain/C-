49b9a27d618b4b35a41dd7b181acbe4e63c1f6cd
GaelCobain <fmunmar310@g.educaand.es>
2023-09-21 09:13:03 +0200

    última modificación con vowelChain incompleto

---
+++ LongestVowelChain/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int Solve(string str)
+    {
+        string longest = "";
+        int longestLenth = 0;
+        var vowelsList = new List<Char> { 'a','e','i','o','u' };
+        for (int i = 0; i < str.Length; i++)
+            if (vowelsList.Contains(str[i]))
+                System.Console.WriteLine(str[i]);
+
+        throw new NotImplementedException();
+    }
+    public static void Main ()
+    {
+        string str = "ajsdhadhisaod";
+        Solve(str);
+    }
+}
