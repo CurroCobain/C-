e55efe8b89cc730c0b8ba994fad55ea515ec906f
GaelCobain <fmunmar310@g.educaand.es>
2023-09-21 09:11:26 +0200

    Iniciales añadido

---
+++ Iniciales/Program.cs
@@ -0,0 +1,21 @@
+﻿public partial class Kata
+{
+    public static string MakeString(string s)
+    {
+        string myStr = "";
+
+        if (s[0] != ' ')
+        {
+            myStr += s[0];
+        }
+
+        for (int i = 1; i < s.Length - 1; i++)
+        {
+            if (s[i] == ' ')
+            {
+                myStr += s[i + 1];
+            }
+        }
+        return myStr;
+    }
+}
